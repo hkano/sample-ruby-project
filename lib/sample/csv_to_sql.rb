4d173df7a100d82de434c0244c4bddd9b0974986
Hideaki Kano (@hkano) <kano.hideaki@gmail.com>
2018-06-07 15:21:25 +0900

    update CsvToSql

--- lib/sample/csv_to_sql.rb
+++ lib/sample/csv_to_sql.rb
@@ -4,17 +4,33 @@ module Sample
   class CsvToSql
 
     OUTPUTS_DIR = "./files/outputs/"
-    INSERT_TABLE_NAME = "xxx"
+    INSERT_TABLE_NAME = "table_name"
 
     def initialize(path)
       @path = path
     end
 
     def execute
+      before_id = 0
+      values = []
+
       CSV.foreach(@path) do |row|
-        File.open(output_path, "a") do |file|
-          file.write("INSERT INTO #{INSERT_TABLE_NAME} (column1, column2, column3) VALUES (#{row[0]}, #{row[1]}, #{row[2]});\n")
+        next if row[0].to_i.zero?
+
+        if before_id != row[0].to_i
+          if values.length > 0
+            write_file insert_sql(values)
+            values = []
+          end
+          write_file delete_sql(row[0])
+          before_id = row[0].to_i
         end
+        values.push("(#{row[0]}, #{row[1]}, #{row[2]}, #{row[3]}, now(), now())")
+      end
+
+      if values.length > 0
+        write_file insert_sql(values)
+        values = []
       end
     end
 
@@ -24,5 +40,18 @@ module Sample
       OUTPUTS_DIR + File.basename(@path, ".*") + ".sql"
     end
 
+    def write_file(text)
+      File.open(output_path, "a") do |file|
+        file.write text
+      end
+    end
+
+    def insert_sql(values)
+      "INSERT INTO\n  `#{INSERT_TABLE_NAME}` (`id`, `column_1`, `column_2`, `column_3`, `created_at`, `updated_at`)\nVALUES\n  #{values.join(",\n  ")};\n\n"
+    end
+
+    def delete_sql(id)
+      "DELETE FROM `#{INSERT_TABLE_NAME}` WHERE `id` = #{id};\n"
+    end
   end
 end
